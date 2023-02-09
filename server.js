45c849d370298ed5d89ca5c726fbd9b925c537bb
John Xander <johnblox5@gmail.com>
2023-02-09 15:53:37 +0000

    add connectDB to server

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
+const { logEvents } = require('./middleware/logger')
 const PORT = process.env.PORT || 3500
 
-// added connection string to .env file
+connectDB()
 
 app.use(logger)
 
@@ -36,4 +39,12 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
