0513112eeffe66eb42c29f1c85bdb13a14177d82
John Xander <johnblox5@gmail.com>
2023-02-10 16:51:00 +0000

    update deleteUser logic

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,7 +9,7 @@ const bcrypt = require('bcrypt')
 const getAllUsers = asyncHandler(async (req, res) => {
     const users = await User.find().select('-password').lean()
 
-    if (!users) {
+    if (!users?.length) {
         return res.status(400).json({ message: 'No users found'})
     }
 
@@ -99,9 +99,9 @@ const deleteUser = asyncHandler(async (req, res) => {
         return res.status(400).json({ message: 'User ID Required'})
     }
 
-    const notes = await Note.findOne({ user: id }).lean().exec()
+    const note = await Note.findOne({ user: id }).lean().exec()
 
-    if (notes?.length) {
+    if (note) {
         return res.status(400).json({ message: 'User has assigned notes'})
     }
 
